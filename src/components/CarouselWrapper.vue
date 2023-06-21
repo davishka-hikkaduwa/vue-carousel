c91a2498b8e645544950d128163aa97ff69ec592
davishka-hikkaduwa <sanjaya.hikkaduwa@outlook.com>
2023-06-21 02:07:48 +0200

    carousel function logic

--- src/components/CarouselWrapper.vue
+++ src/components/CarouselWrapper.vue
@@ -1,11 +1,24 @@
 <template>
     <div class="carousel-wrapper">
-        <slot />
+        <slot :currentSlide="currentSlide" />
+
+        <!-- navigation -->
+        <div class="navigate">
+            <div class="toggle-page left"></div>
+        </div>
     </div>        
 </template>
 
 <script>
-export default {};
+import { ref } from "vue";
+
+export default {
+    setup() {
+        const currentSlide = ref(1);
+
+        return { currentSlide }
+    }
+};
 
 </script>
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="home">
-    <CarouselWrapper class="carousel-wrapper">
-      <CarouselSlide>
-        <div>
-          <p>HELLO</p>
+    <CarouselWrapper class="carousel-wrapper" v-slot="{ currentSlide }">
+      <CarouselSlide v-for="(slide, index) in carouselSlides" :key="index">
+        <div class="slide-info" v-show="currentSlide === index + 1">
+          <img :src="require(`../assets/${slide}.jpg`)" alt="">
         </div>
       </CarouselSlide>
     </CarouselWrapper>
@@ -19,13 +19,34 @@ export default {
   name: 'HomeView',
   components: {
     CarouselWrapper, CarouselSlide
-  }
+  },
+  setup() {
+    const carouselSlides = ["bg-1", "bg-2", "bg-3"];
+
+    return { carouselSlides };
+  },
 }
 </script>
 
 <style lang="scss" scoped>
-  .carousel-wrapper{
+  .carousel-wrapper {
+    position: relative;
     max-height: 100vh;
     height: 100vh;
+
+    .slide-info {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      max-height: 100%;
+      height: 100%;
+
+      img {
+        min-width: 100%;
+        height: 100%;
+        object-fit: cover;
+      }
+    }
   }
 </style>
